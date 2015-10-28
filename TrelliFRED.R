98a06b162f17f567bf4009fccb6ebcf95e63f94d
jhamski <hamski.2@gmail.com>
2015-10-27 21:10:15 -0400

    First commit.

---
+++ TrelliFRED.R
@@ -0,0 +1,5 @@
+#Goal: use the Trelliscope package to visualize data from FRED
+#Trelliscope: http://tessera.io/docs-trelliscope/#quickstart
+#Get FRED data from quantmod: http://www.quantmod.com/documentation/getSymbols.FRED.html
+
+library(quantmod)
